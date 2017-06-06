36ad7a424ff348e3f529ac674b13c5b039597f9a
Camila <camilagm93@gmail.com>
2017-06-06 13:06:27 +0200

    2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,71 +6,117 @@ var paginate = require('../helpers/paginate').paginate;
 // Autoload el quiz asociado a :quizId
 exports.load = function (req, res, next, quizId) {
 
-    models.Quiz.findById(quizId)
-    .then(function (quiz) {
-        if (quiz) {
-            req.quiz = quiz;
-            next();
-        } else {
-            throw new Error('No existe ningún quiz con id=' + quizId);
-        }
+    models.Quiz.findById(quizId, {
+        include: [
+            models.Tip,
+            {model: models.User, as: 'Author'}
+        ]
     })
-    .catch(function (error) {
-        next(error);
-    });
+        .then(function (quiz) {
+
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            } else {
+                throw new Error('No existe ningún quiz con id=' + quizId);
+            }
+        })
+        .catch(function (error) {
+            next(error);
+        });
 };
 
 
 // GET /quizzes
 exports.index = function (req, res, next) {
 
-    var countOptions = {};
+    var countOptions = {
+        where: {}
+    };
+
+    var title = "Preguntas";
 
     // Busquedas:
     var search = req.query.search || '';
     if (search) {
         var search_like = "%" + search.replace(/ +/g,"%") + "%";
 
-        countOptions.where = {question: { $like: search_like }};
+        countOptions.where.question = { $like: search_like };
+    }
+
+    // Si existe req.user, mostrar solo sus preguntas.
+    if (req.user) {
+        countOptions.where.AuthorId = req.user.id;
+        title = "Preguntas de " + req.user.username;
     }
 
     models.Quiz.count(countOptions)
-    .then(function (count) {
+        .then(function (count) {
 
-        // Paginacion:
+            var items_per_page = 10;
 
-        var items_per_page = 10;
 
-        // La pagina a mostrar viene en la query
-        var pageno = parseInt(req.query.pageno) || 1;
+            var page = parseInt(req.query.page) || 1;
 
-        // Crear un string con el HTML que pinta la botonera de paginacion.
-        // Lo añado como una variable local de res para que lo pinte el layout de la aplicacion.
-        res.locals.paginate_control = paginate(count, items_per_page, pageno, req.url);
 
-        var findOptions = countOptions;
+            res.locals.paginate_control = paginate(count, items_per_page, page, req.url);
 
-        findOptions.offset = items_per_page * (pageno - 1);
-        findOptions.limit = items_per_page;
+            var findOptions = countOptions;
 
-        return models.Quiz.findAll(findOptions);
-    })
-    .then(function (quizzes) {
-        res.render('quizzes/index.ejs', {
-            quizzes: quizzes,
-            search: search
+            findOptions.offset = items_per_page * (page - 1);
+            findOptions.limit = items_per_page;
+            findOptions.include = [{model: models.User, as: 'Author'}];
+
+            return models.Quiz.findAll(findOptions);
+        })
+        .then(function (quizzes) {
+
+            res.render('quizzes/index.ejs', {
+                quizzes: quizzes,
+                search: search,
+                title: title
+            });
+        })
+        .catch(function (error) {
+            next(error);
         });
-    })
-    .catch(function (error) {
-        next(error);
-    });
 };
+
 
 
 // GET /quizzes/:quizId
 exports.show = function (req, res, next) {
 
-    res.render('quizzes/show', {quiz: req.quiz});
+    // Antes de renderizar quizzes/show con req.quiz,
+    // necesitamos modificar req.quiz para que las tips
+    // tengan el campo "username"
+
+    var authorId2username = {};
+
+    var allAuthorIdsToConsult = req.quiz.Tips.map(function (tip) {
+        return tip.AuthorId
+    });
+
+    // https://stackoverflow.com/questions/1960473/unique-values-in-an-array
+    var uniqueAuthorIdsToConsult = allAuthorIdsToConsult.filter(function(value, index, self){
+        return self.indexOf(value) === index;
+    });
+
+    var consultPromises = uniqueAuthorIdsToConsult.map(function(AuthorId){
+        return userController.getUsernameForAuthorId(AuthorId, authorId2username)
+    });
+
+    // modifyQuizTipsAddingUsername(req.quiz.Tips, authorId2username);
+
+    Promise.all(consultPromises)
+        .then(function(){
+
+            req.quiz.Tips.forEach(function(tip){
+                tip.username = authorId2username[tip.AuthorId.toString()];
+            });
+
+            res.render('quizzes/show', {quiz: req.quiz});
+        });
 };
 
 
@@ -83,33 +129,50 @@ exports.new = function (req, res, next) {
 };
 
 
+
 // POST /quizzes/create
 exports.create = function (req, res, next) {
 
+    var authorId = req.session.user && req.session.user.id || 0;
+
     var quiz = models.Quiz.build({
         question: req.body.question,
-        answer: req.body.answer
+        answer: req.body.answer,
+        AuthorId: authorId
     });
 
     // guarda en DB los campos pregunta y respuesta de quiz
-    quiz.save({fields: ["question", "answer"]})
-    .then(function (quiz) {
-        req.flash('success', 'Quiz creado con éxito.');
-        res.redirect('/quizzes/' + quiz.id);
-    })
-    .catch(Sequelize.ValidationError, function (error) {
+    quiz.save({fields: ["question", "answer", "AuthorId"]})
+        .then(function (quiz) {
+            req.flash('success', 'Quiz creado con éxito.');
+            res.redirect('/quizzes/' + quiz.id);
+        })
+        .catch(Sequelize.ValidationError, function (error) {
 
-        req.flash('error', 'Errores en el formulario:');
-        for (var i in error.errors) {
-            req.flash('error', error.errors[i].value);
-        }
+            req.flash('error', 'Errores en el formulario:');
+            for (var i in error.errors) {
+                req.flash('error', error.errors[i].value);
+            }
 
-        res.render('quizzes/new', {quiz: quiz});
-    })
-    .catch(function (error) {
-        req.flash('error', 'Error al crear un Quiz: ' + error.message);
-        next(error);
-    });
+            res.render('quizzes/new', {quiz: quiz});
+        })
+        .catch(function (error) {
+            req.flash('error', 'Error al crear un Quiz: ' + error.message);
+            next(error);
+        });
+};
+
+
+exports.adminOrAuthorRequired = function(req, res, next){
+
+    var isAdmin  = req.session.user.isAdmin;
+    var isAuthor = req.quiz.AuthorId === req.session.user.id;
+
+    if (isAdmin || isAuthor) {
+        next();
+    } else {
+        res.send(403);
+    }
 };
 
 
@@ -127,23 +190,23 @@ exports.update = function (req, res, next) {
     req.quiz.answer = req.body.answer;
 
     req.quiz.save({fields: ["question", "answer"]})
-    .then(function (quiz) {
-        req.flash('success', 'Quiz editado con éxito.');
-        res.redirect('/quizzes/' + req.quiz.id);
-    })
-    .catch(Sequelize.ValidationError, function (error) {
+        .then(function (quiz) {
+            req.flash('success', 'Quiz editado con éxito.');
+            res.redirect('/quizzes/' + req.quiz.id);
+        })
+        .catch(Sequelize.ValidationError, function (error) {
 
-        req.flash('error', 'Errores en el formulario:');
-        for (var i in error.errors) {
-            req.flash('error', error.errors[i].value);
-        }
+            req.flash('error', 'Errores en el formulario:');
+            for (var i in error.errors) {
+                req.flash('error', error.errors[i].value);
+            }
 
-        res.render('quizzes/edit', {quiz: req.quiz});
-    })
-    .catch(function (error) {
-        req.flash('error', 'Error al editar el Quiz: ' + error.message);
-        next(error);
-    });
+            res.render('quizzes/edit', {quiz: req.quiz});
+        })
+        .catch(function (error) {
+            req.flash('error', 'Error al editar el Quiz: ' + error.message);
+            next(error);
+        });
 };
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var tipController = require('../controllers/tip_controller');
+var userController = require('../controllers/user_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,19 +19,56 @@ router.get('/author', function(req, res, next) {
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
+router.param('userId', userController.load);
+router.param('tipId',  tipController.load);
 
+
+// Definición de rutas de sesion
+router.get('/session', sessionController.new);
+router.post('/session', sessionController.create);
+router.delete('/session', sessionController.destroy);
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizzes', sessionController.loginRequired, quizController.create);
+router.get('/quizzes/:quizId(\\d+)/tips/new', sessionController.loginRequired, tipController.new);
+router.post('/quizzes/:quizId(\\d+)/tips', sessionController.loginRequired, tipController.create);
+router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)', sessionController.loginRequired, tipController.destroy);
+router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/accept', sessionController.loginRequired, quizController.adminOrAuthorRequired, tipController.accept);
+router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.adminOrAuthorRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.adminOrAuthorRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.adminOrAuthorRequired, quizController.destroy);
+
+
+// Definición de rutas de cuenta
+router.get('/users',
+    sessionController.loginRequired,
+    userController.index);
+router.get('/users/:userId(\\d+)',
+    sessionController.loginRequired,
+    userController.show);
+router.get('/users/new',
+    userController.new);
+router.post('/users',
+    userController.create);
+router.get('/users/:userId(\\d+)/edit',
+    sessionController.loginRequired,
+    sessionController.adminOrMyselfRequired,
+    userController.edit);
+router.put('/users/:userId(\\d+)',
+    sessionController.loginRequired,
+    sessionController.adminOrMyselfRequired,
+    userController.update);
+router.delete('/users/:userId(\\d+)',
+    sessionController.loginRequired,
+    sessionController.adminOrMyselfRequired,
+    userController.destroy);
+
+router.get('/users/:userId(\\d+)/quizzes', quizController.index);     // ver las preguntas de un usuario
 
 
 module.exports = router;
